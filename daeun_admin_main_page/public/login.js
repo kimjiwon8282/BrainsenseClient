10aa3f3a356de848adcbd17397e4fe648b0d6b10
정다은 <ekdms8652@pusan.ac.kr>
2025-02-10 14:00:53 +0900

    관리자 로그인 페이지 1차

---
+++ daeun_admin_main_page/public/login.js
@@ -0,0 +1,30 @@
+document
+  .getElementById('loginForm')
+  .addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      const response = await fetch('http://localhost:8081/admin/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem('token', data.token);
+        window.location.href = '/admin/dashboard';
+      } else {
+        document.getElementById('errorMessage').innerText = data.message;
+      }
+    } catch (error) {
+      console.error('로그인 에러', error);
+      document.getElementById('errorMessage').innerText = '서버 오류';
+    }
+  });
