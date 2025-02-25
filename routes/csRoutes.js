62037e02b7804230da774aaa8ef90d5104b2035d
정다은 <ekdms8652@pusan.ac.kr>
2025-02-25 17:27:58 +0900

    merge 전 수정

--- routes/csRoutes.js
+++ routes/csRoutes.js
@@ -6,18 +6,8 @@ router.get('/ask', (req, res) => {
   res.render('csAsk.ejs');
 });
 
-// 뉴스 상세 페이지
-router.get('/company/news/:id', async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-      return res.status(404).send('게시글을 찾을 수 없습니다.');
-    }
-    res.render('client_cs_detail', { post });
-  } catch (err) {
-    console.error('게시글 조회 오류:', err);
-    res.status(500).send('서버 오류');
-  }
+router.get('/faq', (req, res) => {
+  res.render('csFaq.ejs');
 });
 
 module.exports = router;
