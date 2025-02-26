6f8656d3026e577675010ba8c6c92b2a176f7a0b
정다은 <ekdms8652@pusan.ac.kr>
2025-02-26 10:45:13 +0900

    company/intro 완료

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-router.get('/intro',(req,res)=>{
-    res.send('인트로 페이지');
+router.get('/intro', (req, res) => {
+  res.render('companyIntro');
 });
 
-router.get('/news',(req,res)=>{
-    res.send('뉴스 페이지');
+router.get('/news', (req, res) => {
+  res.send('뉴스 페이지');
 });
 
-router.get('/team',(req,res)=>{
-    res.send('팀소개 페이지');
+router.get('/team', (req, res) => {
+  res.send('팀소개 페이지');
 });
 
 module.exports = router;
