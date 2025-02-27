652f82c6932e0663de94a3bec2f70a420ffcfa6a
정다은 <ekdms8652@pusan.ac.kr>
2025-02-27 21:33:51 +0900

    product/hw, sw 프론트 완료

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/hw', (req, res) => {
-  res.send('HW 페이지');
+  res.render('productHw');
 });
 
 router.get('/sw', (req, res) => {
-  res.send('SW 페이지');
+  res.render('productSw');
 });
 
 module.exports = router;
