acd62d5812c854137ccd55781a4ca9042e6cf34d
정다은 <ekdms8652@pusan.ac.kr>
2025-03-30 13:52:55 +0900

    페이지 준비중 완료

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.render('productHw');
+});
+
 router.get('/hw', (req, res) => {
   res.render('productHw');
 });
