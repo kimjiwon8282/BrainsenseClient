860ca9f891775e76134d0c027ed58eee02746c63
ekdmssld <ekdms8652@gmail.com>
2025-05-11 19:39:43 +0900

    수정본

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: true, // 세미콜론 사용
-  singleQuote: true, // 작은따옴표 사용
-  trailingComma: 'es5', // ES5 호환 방식으로 마지막에 쉼표 추가
-  htmlWhitespaceSensitivity: 'ignore',
-  htmlWhitespaceSensitivity: 'css', // HTML에서 공백 처리 무시
-};
