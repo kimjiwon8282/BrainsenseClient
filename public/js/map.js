1856310e637377f3fbe80097a780f49cec3b6ac7
ekdmssld <ekdms8652@gmail.com>
2025-05-11 22:13:15 +0900

    지도 marker 설정

--- public/js/map.js
+++ public/js/map.js
@@ -5,3 +5,13 @@ var options = {
 };
 
 var map = new kakao.maps.Map(container, options);
+
+var markerPosition = new kakao.maps.LatLng(
+  35.451757048437976,
+  128.809566612766
+);
+var marker = new kakao.maps.Marker({
+  position: markerPosition,
+});
+
+marker.setMap(map);
